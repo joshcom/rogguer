05777fafe1a61d6a6cd8228d47f59016502474d3
Joshua Murray <jmurray@sparkplatform.com>
2014-06-08 10:20:43 -0500

    Draw status bars. Text updates.

--- lib/rogguer/console/curses_console.rb
+++ lib/rogguer/console/curses_console.rb
@@ -28,6 +28,10 @@ module Rogguer
         line_num   = 0
         column_num = 0
 
+        map_width = map.structure.first.length
+
+        line_num = draw_top_status_bar(status_bar, line_num, map_width)
+
         map.structure.each do |line| 
           line.each do |character|
             Curses.setpos(line_num, column_num)
@@ -38,7 +42,39 @@ module Rogguer
           column_num = 0
         end
 
+        line_num = draw_bottom_status_bar(status_bar, line_num, map_width)
+
         Curses.refresh
+      end
+
+      def draw_top_status_bar(status_bar, current_line_number, width)
+        title = status_bar.title_text
+        level = status_bar.level_text
+        text = fill_text_with_padding(title, level, width)
+        draw_status_bar_text(text, current_line_number)
+      end
+
+      def draw_bottom_status_bar(status_bar, current_line_number, width)
+        lives = status_bar.lives_text
+        steps = status_bar.steps_text
+        text = fill_text_with_padding(lives, steps, width)
+        draw_status_bar_text(text, current_line_number)
+      end
+
+      def draw_status_bar_text(text, current_line_number)
+        Curses.setpos(current_line_number, 0)
+        Curses.addstr(text)
+        current_line_number += 1
+      end
+
+      def fill_text_with_padding(left_text, right_text, width)
+        padding_needed = width - (left_text.length + right_text.length) 
+        padding_needed = 0 if padding_needed < 0
+
+        padding_text = ""
+        padding_needed.times { |x| padding_text += " " }
+
+        "#{left_text}#{padding_text}#{right_text}"
       end
 
       def last_command
